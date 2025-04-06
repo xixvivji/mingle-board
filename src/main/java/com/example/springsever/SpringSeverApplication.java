21ad25482ea537e9986a92a8b65fe728e2074252
xixvivji <kjw3568@naver.com>
2025-04-07 00:05:39 +0900

    Initial commit

---
+++ src/main/java/com/example/springsever/SpringSeverApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSeverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsever/SpringSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
