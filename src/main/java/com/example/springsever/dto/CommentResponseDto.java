6fd153b72f55a62158519f566dd5fec9711f526a
xixvivji <kjw3568@naver.com>
2025-06-10 23:00:33 +0900

    #061003

--- src/main/java/com/example/springsever/dto/CommentResponseDto.java
+++ src/main/java/com/example/springsever/dto/CommentResponseDto.java
@@ -1,4 +1,14 @@
 package com.example.springsever.dto;
 
+import com.example.springsever.entity.Comment;
+import lombok.Getter;
+
+@Getter
 public class CommentResponseDto {
+    private final String content;
+
+    public CommentResponseDto(Comment comment) {
+        this.content = comment.getContent();
+
+    }
 }
