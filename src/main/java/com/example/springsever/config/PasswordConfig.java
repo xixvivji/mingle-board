789908e4e693f12c55f55fa3afa68e94d297bcee
xixvivji <kjw3568@naver.com>
2025-06-19 23:32:53 +0900

    #061901

---
+++ src/main/java/com/example/springsever/config/PasswordConfig.java
@@ -0,0 +1,14 @@
+package com.example.springsever.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
