1050467374321a7da68f08de4e35b1c3b6a501e6
xixvivji <kjw3568@naver.com>
2025-06-20 23:31:16 +0900

    #062003

--- src/main/java/com/example/springsever/repository/PostRepository.java
+++ src/main/java/com/example/springsever/repository/PostRepository.java
@@ -7,8 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByCreatedAtDesc();
 
+    // List<Post> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+    // UserId로 정렬하는 메서드는 현재 사용하지 않지만, 필요시 주석 해제하여 사용 가능
+
+    //List<Post> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+    // UserId로 정렬하는 메서드는 현재 사용하지 않지만, 필요시 주석 해제하여 사용 가능
+
     // 두메서드다 JpaRepository에서 제공하는 메서드로, 명시적으로 다시 선언할 필요X
     //Post save(Post post);
-
     //Optional<Post> findById(Long id);
 }
