2e2641dc54dc850058a5f882e1aacf4e18b61b29
xixvivji <kjw3568@naver.com>
2025-06-20 23:34:56 +0900

    #062004

--- src/main/java/com/example/springsever/dto/StatusCodeResponseDto.java
+++ src/main/java/com/example/springsever/dto/StatusCodeResponseDto.java
@@ -12,4 +12,9 @@ public class StatusCodeResponseDto {
         this.statusCode = statusCode;
     }
 
+    //StausCodeResponseDto.builder()
+    //     .statusCode(statusCode)
+    //     .message("로그인 성공")
+    //     .build();
+    //단, Builder는 필요할 때만 쓰는 걸 권장(간단한 클래스에는 불필요할 수도 있음).
 }
